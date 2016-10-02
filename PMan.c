e46ac1decce275123003be657e4bd52c3c9db26a
Jose Gordillo <gordillo@uvic.ca>
2016-10-02 14:25:31 -0700

    moved back all code into one file

--- PMan.c
+++ PMan.c
@@ -7,12 +7,18 @@
 #include <sys/wait.h>
 #include <sys/types.h> 
 #include <errno.h>
-#include "process_status.h"
-#include "proc_list.h"
 
 #define MAX_LINE_LEN 256
 #define TOK_BUF_SIZE 64
 #define MAX_PATH_LEN 40
+
+typedef struct Proc { // define struct to hold info about process
+	pid_t pid;
+	char * state;
+	int isStop;
+	char * cmd;
+	struct Proc * next;
+} Proc;
 
 // prototypes for all the functions in this file
 char * prompt_user();
@@ -22,6 +28,17 @@ int execute_process(char ** args, char * line);
 int handle_user_input(char ** args, char * line);
 void change_process_status(char * pid, int option);
 void update_bg_procss();
+void find_and_print_process_info(pid_t target_pid);
+void print_utime_and_stime(pid_t target_pid);
+typedef struct Proc Proc;
+void *emalloc(int n);
+Proc *new_item (pid_t pid, char * line);
+Proc *add_front(Proc *listp, Proc *newp);
+Proc *delete_item (Proc *listp, pid_t pid);
+void update_isStop(Proc *listp, pid_t pid, int val);
+int lookup_pid(Proc *listp, pid_t pid);
+void print_all(Proc *listp);
+void free_all(Proc *listp);
 
 // global pointer responsible for keeping track of the processes created/removed
 Proc * process_list;
@@ -217,8 +234,6 @@ void print_utime_and_stime(pid_t target_pid) {
 		return;
 	}
 
- 	printf("PID: %d\n", (int) target_pid);
-
   if (fgets(line, MAX_LINE_LEN, status_file)) { // get first 256 chars from the first line
   	char ** proc_stat = parse_user_input(line);
 		long utime = atol(*(proc_stat + 14)) / sysconf(_SC_CLK_TCK);
@@ -231,6 +246,169 @@ void print_utime_and_stime(pid_t target_pid) {
   fclose(status_file);
 }
 
+void find_and_print_process_info(pid_t target_pid) {
+	char path[MAX_PATH_LEN], line[MAX_LINE_LEN], *text;
+  FILE* status_file;
+
+  snprintf(path, MAX_PATH_LEN, "/proc/%d/status", (int) target_pid); // setup path to file
+
+  status_file = fopen(path, "r"); // read file
+
+  if(!status_file) { // error handling
+		perror("Could not open status file.\n");
+		return;
+	}
+
+ 	printf("PID: %d\n", (int) target_pid);
+
+  while(fgets(line, MAX_LINE_LEN, status_file)) { // read line by line
+
+		// get name info 
+  	if(strncmp(line, "Name:", 5) == 0) {
+			// Ignore "Name:" and whitespace
+			text = line + 6;
+			while(isspace(*text)) ++text;
+
+			printf("comm: %s", text);
+		}
+
+		// get state info
+		if(strncmp(line, "State:", 6) == 0) {
+			// Ignore "State:" and whitespace
+			text = line + 7;
+			while(isspace(*text)) ++text;
+
+			printf("state: %s", text);
+		}
+		
+		// get rss info
+		if(strncmp(line, "VmRSS:", 6) == 0) {
+			// Ignore "VmRSS:" and whitespace
+			text = line + 7;
+			while(isspace(*text)) ++text;
+
+			printf("rss: %s", text);
+		}
+		
+		// get voluntary cxtx switches info
+		if(strncmp(line, "voluntary_ctxt_switches:", 24) == 0) {
+			// Ignore "voluntary_ctxt_switches:" and whitespace
+			text = line + 25;
+			while(isspace(*text)) ++text;
+
+			printf("voluntary_ctxt_switches: %s", text);
+		}
+		
+		// get nonvoluntary cxtx switches info
+		if(strncmp(line, "nonvoluntary_ctxt_switches:", 27) == 0) {
+			// Ignore "nonvoluntary_ctxt_switches:" and whitespace
+			text = line + 28;
+			while(isspace(*text)) ++text;
+
+			printf("nonvoluntary_ctxt_switches: %s", text);
+		}
+  }
+
+  fclose(status_file);
+}
+
+void *emalloc(int n) { // custom malloc function with error checking
+ 	void *p;
+ 	p = malloc(n);
+ 	if (p == NULL) { // error handling
+	 	perror("command malloc failed\n");
+		exit(EXIT_FAILURE);
+ 	}
+ 	return p;
+}
+
+// creates a new node for the process list
+Proc *new_item (pid_t pid, char * line) {
+ 	Proc *newp;
+ 	newp = (Proc *) emalloc(sizeof(Proc));
+ 	newp->pid = pid;
+ 	newp->state = NULL;
+ 	newp->isStop = 0;
+ 	newp->next = NULL;
+ 	newp->cmd = (char*) emalloc(strlen(line)*sizeof(char)); // allocates space for cmd
+ 	strcpy(newp->cmd, line); // gets a copy of what the user entered
+ 	return newp;
+}
+
+// adds a node at the front of the list
+Proc *add_front(Proc *listp, Proc *newp) {
+ 	newp->next = listp;
+ 	return newp;
+}
+
+// deletse node with the given pid
+Proc *delete_item (Proc *listp, pid_t pid) {
+ 	Proc *curr, *prev, *head;
+ 	head = listp;
+ 	prev = NULL;
+	
+ 	for (curr = listp; curr != NULL; curr = curr->next) {
+ 		if (pid == curr->pid) {
+ 			if (curr->next == NULL && prev == NULL) {
+				// deallocate relevant memory
+				if (curr->cmd != NULL) free(curr->cmd);
+	 			free(curr);
+	 			return NULL;
+ 			}
+			else if (prev == NULL) listp = curr->next;
+			else prev->next = curr->next;
+ 			
+ 			if (curr->cmd != NULL) free(curr->cmd);
+			// deallocate relevant memory
+ 			free(curr);
+ 			return head;
+ 		}
+ 		prev = curr;
+ 	}
+ 	return head;
+}
+
+// updates the isStop prop of the struct
+void update_isStop(Proc *listp, pid_t pid, int val) {
+	Proc * tmp = listp;
+ 	for (; tmp != NULL; tmp = tmp->next) {
+ 		if (pid == tmp->pid) {
+ 			tmp->isStop = val;
+ 			break;
+ 		}
+ 	}
+}
+
+// checks to see if process with given pid is in process list
+int lookup_pid(Proc *listp, pid_t pid) {
+	Proc * tmp = listp;
+ 	for (; tmp != NULL; tmp = tmp->next) if (pid == tmp->pid) return 1;
+ 	return 0;
+}
+
+// prints all the bg processes
+void print_all(Proc *listp) {
+	int i = 0;
+ 	Proc *next;
+ 	while (listp != NULL) {
+ 		if (!listp->isStop) {
+ 			printf("PMan:> %d: %s\n", (int) listp->pid, listp->cmd);
+			i++;
+ 		}
+		listp = listp->next;
+ 	}
+	printf("PMan:> Total background jobs: %d\n", i);
+}
+
+// deallocates all the nodes in the process list
+void free_all(Proc *listp) {
+ 	Proc *next;
+ 	for ( ; listp != NULL; listp = next ) {
+ 		next = listp->next;
+		if (listp->cmd != NULL) free(listp->cmd);
+ 		free(listp);
+ 	}
+}
 
 
 
